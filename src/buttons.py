8f6c8c75b2a88711016c01bd72fff6d24756bcca
FilipOstarek <46921112+FilipOstarek@users.noreply.github.com>
2021-01-18 17:20:30 +0100

    Add files via upload

---
+++ src/buttons.py
@@ -0,0 +1,31 @@
+import pygame
+import pickle
+import random
+import math
+
+
+class Buttons(pygame.sprite.Sprite):
+    def __init__(self, color, x, y, width, height, text):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+    def draw(self, screen, outline=None):
+        if outline:
+            pygame.draw.rect(screen, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
+
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+
+        if self.text != '':
+            font = pygame.font.SysFont('calibri', 30)
+            text = font.render(self.text, True, (0,0,0))
+            screen.blit(text, (self.x + (self.width/10 + text.get_width()/10), self.y + (self.height/2 - text.get_height()/10)))
+
+    def isOver(self, pos):
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+
+        return False
